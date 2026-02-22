2919b85cac8d0b0875f82d7dd3024e3225c56e29
David Santiago Viloria <140551397+Davidsvr04@users.noreply.github.com>
2026-02-22 10:29:35 -0500

    Creación clase homeController
    Se crea controlador para retornar mensaje

---
+++ ITM.ProyectoDespliegue/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITM.ProyectoDespliegue.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World desde la nueva funcionalidad");
+        }
+    }
+}
